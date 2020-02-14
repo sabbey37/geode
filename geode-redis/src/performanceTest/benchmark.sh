ea13a711c4f3874d6e91b29037863e82a8cfc296
John Hutchison <jhutchison@pivotal.io>
2020-02-14 17:06:03 -0500

    start performance test

---
+++ geode-redis/src/performanceTest/benchmark.sh
@@ -0,0 +1,15 @@
+#start geode redis
+#start redis becnhmark and listen to gedoe redis API
+
+gfsh -e "start locator"
+
+gfsh -e "start server
+          --name=server1
+         --locators=localhost[10334]
+          --server-port=0
+          --redis-port=6379
+          --redis-bind-address=127.0.0.1"
+
+redis-benchmark -t set, get -n 10000 -q --csv
+
+gfsh -e "connect shutdown include-locators=true"
