45a1c5cd26302a299fbf6104cb5aeed9f0cf3c22
Sarah <sabbey@pivotal.io>
2020-02-18 15:57:53 -0500

    WIP - Experimenting with benchmarking

--- geode-redis/src/performanceTest/benchmark.sh
+++ geode-redis/src/performanceTest/benchmark.sh
@@ -12,16 +12,36 @@ echo "comparisonCommit: ${comparisonCommit}"
 originalBranch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 echo " originalBranch: ${originalBranch}"
 
-git checkout ${baseLineCommit}
-returnCode=$?
-echo " returnCode: ${returnCode}"
- if [[ ${returnCode} -ne 0 ]] ; then
-    git stash
-    neededToStash=true
-    git co ${baseLineCommit}
-fi
+#git checkout ${baseLineCommit}
+#returnCode=$?
+#echo " returnCode: ${returnCode}"
+# if [[ ${returnCode} -ne 0 ]] ; then
+#    git stash
+#    neededToStash=true
+#    git co ${baseLineCommit}
+#fi
 
 echo "STASHHHHHH: ${neededToStash}"
+
+gfsh -e "start locator"
+
+gfsh -e "start server
+          --name=server1
+          --locators=localhost[10334]
+          --server-port=0
+          --redis-port=6379
+          --redis-bind-address=127.0.0.1"
+
+redis-benchmark -t set,get -q -n 100000  --csv > results.csv
+
+gfsh -e "connect" -e "shutdown --include-locators=true"
+
+#git checkout ${originalBranch}
+#
+#if [[ ${neededToStash} ]] ; then
+#  echo "POPPING THE STASH"
+#  git stash pop
+#fi
 
 gfsh -e "start locator"
 
@@ -32,13 +52,6 @@ gfsh -e "start server
           --redis-port=6379
           --redis-bind-address=127.0.0.1"
 
-redis-benchmark -t set,get -q -n 10000
+redis-benchmark -t set,get -q -n 100000  --csv > results2.csv
 
 gfsh -e "connect" -e "shutdown --include-locators=true"
-
-git checkout ${originalBranch}
-
-if [[ ${neededToStash} ]] ; then
-  echo "POPPING THE STASH"
-  git stash pop
-fi
