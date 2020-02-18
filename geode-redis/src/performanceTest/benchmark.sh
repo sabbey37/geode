0c0b7cc6d1cad0513462b1f33dc016d00a0cdc3e
John Hutchison <jhutchison@pivotal.io>
2020-02-18 14:31:31 -0500

    WIP-more work on benchmark.sh

--- geode-redis/src/performanceTest/benchmark.sh
+++ geode-redis/src/performanceTest/benchmark.sh
@@ -4,12 +4,23 @@
 
 baseLineCommit=$1
 comparisonCommit=$2
+neededToStash=false
 
 echo "baseLineCommit: ${baseLineCommit}"
 echo "comparisonCommit: ${comparisonCommit}"
 
-git stash
+originalBranch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+echo " originalBranch: ${originalBranch}"
+
+
 git co ${baseLineCommit}
+  returnCode=$?
+echo " returnCode: ${returnCode}"
+# if [[ returnCode -eq 0 ]] ; then
+#    git stash
+#    $neededToStash=true
+#    git co ${baseLineCommit}
+#fi
 
 gfsh -e "start locator"
 
@@ -23,3 +34,5 @@ gfsh -e "start server
 redis-benchmark -t set,get -q -n 10000
 
 gfsh -e "connect" -e "shutdown --include-locators=true"
+
+git co ${originalBranch}
